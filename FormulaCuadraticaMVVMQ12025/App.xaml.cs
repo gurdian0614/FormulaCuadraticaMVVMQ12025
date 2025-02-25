ff5e56196b8b62f4fdb147e278672c0ba45301dd
gurdian0614 <ivan.gurdian@outlook.com>
2025-02-24 19:18:41 -0600

    Invoke View

--- FormulaCuadraticaMVVMQ12025/App.xaml.cs
+++ FormulaCuadraticaMVVMQ12025/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace FormulaCuadraticaMVVMQ12025
+﻿using FormulaCuadraticaMVVMQ12025.Views;
+
+namespace FormulaCuadraticaMVVMQ12025
 {
     public partial class App : Application
     {
@@ -6,7 +8,7 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new FormulaCuadraticaView();
         }
     }
 }
