e6c3feaa7733acdc9f9e33eaeb4ef844520341fd
gurdian0614 <ivan.gurdian@outlook.com>
2025-02-24 18:21:38 -0600

    Add project files.

---
+++ FormulaCuadraticaMVVMQ12025/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace FormulaCuadraticaMVVMQ12025
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ FormulaCuadraticaMVVMQ12025/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace FormulaCuadraticaMVVMQ12025
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
